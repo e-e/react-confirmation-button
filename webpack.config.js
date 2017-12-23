4f1987aafec28ae1e8cec0ebaf30fae29b895a21
Eric Ellingson <ellingsonster@gmail.com>
2017-12-22 19:35:26 -0800

    .

--- webpack.config.js
+++ webpack.config.js
@@ -12,31 +12,11 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          require.resolve('style-loader'),
           {
-            loader: require.resolve('css-loader'),
+            loader: 'css-loader',
             options: {
-              importLoaders: 1
-            }
-          },
-          {
-            loader: require.resolve('postcss-loader'),
-            options: {
-              // Necessary for external CSS imports to work
-              // https://github.com/facebookincubator/create-react-app/issues/2677
-              ident: 'postcss',
-              plugins: () => [
-                require('postcss-flexbugs-fixes'),
-                autoprefixer({
-                  browsers: [
-                    '>1%',
-                    'last 4 versions',
-                    'Firefox ESR',
-                    'not ie < 9' // React doesn't support IE8 anyway
-                  ],
-                  flexbox: 'no-2009'
-                })
-              ]
+              modules: true,
+              localIdentName: '[path][name]__[local]--[hash:base64:5]'
             }
           }
         ]
